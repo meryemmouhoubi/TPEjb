9530f60ccd2d1916164dea66098467ceef34c77f
meryem <maryammouhoubi@gmail.com>
2023-11-10 21:19:52 +0100

    client

--- G4Client/src/TestEmploye.java
+++ G4Client/src/TestEmploye.java
@@ -1,23 +1,24 @@
 import java.util.Hashtable;
 
 
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sound.midi.Soundbank;
 
 import dao.IDao;
+import entities.Etudiant;
 import entities.Filiere;
-import entities.Student;
 
 public class TestEmploye {
-	public static IDao<Student> lookUpStudentRemote() throws NamingException {
+	public static IDao<Etudiant> lookUpStudentRemote() throws NamingException {
 		final Hashtable jndiProperties = new Hashtable();
 		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
 		jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
 		final Context context = new InitialContext(jndiProperties);
 
-		return (IDao<Student>) context.lookup("ejb:/G4Serveur/G4EmS!dao.IDao");
+		return (IDao<Etudiant>) context.lookup("ejb:/G4Serveur/G4EmS!dao.IDao");
 
 	}
 
@@ -35,16 +36,16 @@ public class TestEmploye {
 	
 	public static void main(String[] args) {
 		try {
-			IDao<Student> dao = lookUpStudentRemote();
+			IDao<Etudiant> dao = lookUpStudentRemote();
 			IDao<Filiere> daof = lookUpFiliereRemote();
 			Filiere f1=daof.create(new Filiere(1,"IIR"));
 			Filiere f2=daof.create(new Filiere(2,"GC"));
-			dao.create(new Student("pass1","naja","Mohamed","Naja",f1));
-			dao.create(new Student("pass2","raze","Ahmed","Polan",f1));
-			dao.create(new Student("pass3","plomm","Soufiane","latex",f1));
-			dao.create(new Student("pass4","peterson","Inssaf","Daln",f2));
+			dao.create(new Eudiant("pass1","naja","Mohamed","Naja",f1));
+			dao.create(new Eudiant("pass2","meryem","mouhoubi","Polan",f1));
+			dao.create(new Eudiant("pass3","sana","Soufiane","latex",f1));
+			dao.create(new Eudiant("pass4","laila","Inssaf","Daln",f2));
 			
-			for(Student e: dao.findAll())
+			for(Eudiant e: dao.findAll())
 				System.out.println(e.getFirstName()+"-> filiere : "+e.getFiliere().getName());
 			
 		} catch (NamingException e) {
